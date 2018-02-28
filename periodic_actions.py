15a1afbc5d7be824113ea23559356ad6ad548aab
sociodaxquina <36891021+sociodaxquina@users.noreply.github.com>
2018-02-28 11:52:56 +0000

    Update periodic_actions.py

--- periodic_actions.py
+++ periodic_actions.py
@@ -1,11 +1,20 @@
-
-https://stackoverflow.com/questions/8600161/executing-periodic-actions-in-python
+## periodic actions
+#Link: https://stackoverflow.com/questions/8600161/executing-periodic-actions-in-python
+#Comments: executes foo() every 10 seconds
 
 import time, threading
 import sys
 
 def foo():
     print(time.ctime())
-    threading.Timer(5, foo).start()
+    threading.Timer(10, foo).start()
 
 foo()
+
+#output:
+#   Wed Feb 28 11:10:15 2018
+#   Wed Feb 28 11:10:25 2018
+#   Wed Feb 28 11:10:35 2018
+#   Wed Feb 28 11:10:45 2018
+#   Wed Feb 28 11:10:55 2018
+#   (...)
