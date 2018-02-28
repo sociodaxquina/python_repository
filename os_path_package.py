8dad0ce5c993eb15904ee89ec33a33cf8f189353
sociodaxquina <36891021+sociodaxquina@users.noreply.github.com>
2018-02-28 10:17:03 +0000

    Update os_path_package.py

--- os_path_package.py
+++ os_path_package.py
@@ -1,19 +1,16 @@
 ## os.path package
+#Comments:
+#   __file__ has metadata of the file.py
 
 import os
 
-#__file__ has metadata of the file.py
-
 realpath = os.path.realpath(__file__)
 print(realpath)
-'''
-  > /data/PEDRO/nupic_scripts/testes.py
-'''
+#output: /data/PEDRO/nupic_scripts/testes.py
 
 rootpath = os.path.dirname(os.path.realpath(__file__))
 print(rootpath)
-'''
-  > /data/PEDRO/nupic_scripts/testes.py
-'''
+#output: /data/PEDRO/nupic_scripts/testes.py
+
 
 
