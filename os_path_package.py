11170385c1de0fbfc144e3045b05fc68c0f4c0be
sociodaxquina <36891021+sociodaxquina@users.noreply.github.com>
2018-02-28 10:22:51 +0000

    Update os_path_package.py

--- os_path_package.py
+++ os_path_package.py
@@ -1,16 +1,24 @@
 ## os.path package
-#Comments:
-#   __file__ has metadata of the file.py
+#Link: https://docs.python.org/2/library/os.path.html
+#Comments: __file__ has metadata of the file.py
 
 import os
 
 realpath = os.path.realpath(__file__)
 print(realpath)
-#output: /data/PEDRO/nupic_scripts/testes.py
+#output: /folder1/folder2/folder3/file.py
 
 rootpath = os.path.dirname(os.path.realpath(__file__))
 print(rootpath)
-#output: /data/PEDRO/nupic_scripts/testes.py
+#output: /folder1/folder2/folder3/
+
+basename = os.path.basename(__file__)
+print(basename)
+#output: file.py
+
+create_path = os.path.join('folder1','folder2','file.json')
+print(create_path)
+#output: /folder1/folder2/folder3/file.json
 
 
 
